e39c830673d8f52ed2840df05d727bf599fbd216
Pim Kunis <pim@kunis.nl>
2025-07-05 14:50:40 +0200

    Add more keys

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,8 @@
     [
       # change this to your ssh key
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOodpLr+FDRyKyHjucHizNLVFHZ5AQmE9GmxMnOsSoaw pimkunis@thinkpadpim"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAE0p+dl2wU+zS2lBqnzD6GS1+IjQhwumpvSWpi6eBSxAAAACnNzaDpnaXRodWI="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIB/elARZbd2S351cQX+y2dvs5utOgs6azhh371bGju2BAAAAEHNzaDpub2x0ZS1naXRodWI="
     ]
     ++ (args.extraPublicKeys or []); # this is used for unit-testing this module and can be removed if not needed
 
